ca455eb685068dc9a99e04deff73d3c338a2b63a
Jelle Bruisten <jelle_bruisten@hotmail.com>
2019-12-31 11:16:41 +0100

    - public-api added config

--- projects/angular-heatmap/src/public-api.ts
+++ projects/angular-heatmap/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/angular-heat-map.service';
 export * from './lib/angular-heat-map.module';
 export * from './lib/angular-heat-map.directive';
+export * from './lib/angular-heat-map.config';
