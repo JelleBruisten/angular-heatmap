a03886dad9b0077cf5133c6f6eed3bb702d39d5e
Jelle Bruisten <jelle_bruisten@hotmail.com>
2020-01-06 13:08:58 +0100

    Auto-generated commit

--- projects/angular-heatmap/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of angular-heatmap
- */
-
-export * from './lib/angular-heat-map.service';
-export * from './lib/angular-heat-map.module';
-export * from './lib/angular-heat-map.directive';
-export * from './lib/angular-heat-map.config';
-export * from './lib/angular-heat-map-data';
